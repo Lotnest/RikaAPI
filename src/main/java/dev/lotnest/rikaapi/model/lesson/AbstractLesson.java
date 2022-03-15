cace104d9196eb4b776991bb1882b9fc1e27c8a9
Lotnest <lotnestyt@gmail.com>
2022-03-15 20:52:17 +0100

    Add semester to lesson model

--- src/main/java/dev/lotnest/rikaapi/model/lesson/AbstractLesson.java
+++ src/main/java/dev/lotnest/rikaapi/model/lesson/AbstractLesson.java
@@ -1,6 +1,8 @@
 package dev.lotnest.rikaapi.model.lesson;
 
 import dev.lotnest.rikaapi.enums.EnumLessonType;
+import dev.lotnest.rikaapi.enums.EnumSemester;
+import dev.lotnest.rikaapi.registry.PlanRegistry;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
@@ -11,6 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 public class AbstractLesson {
 
+    private final @NotNull EnumSemester semester = PlanRegistry.CURRENT_SEMESTER;
     private @NotNull LocalDateTime startTime;
     private @NotNull LocalDateTime endTime;
     private @NotNull String code;
