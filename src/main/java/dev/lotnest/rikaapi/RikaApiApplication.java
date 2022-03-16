c6a4348a375468d7dd58c6a8db7ba24877113a6f
Lotnest <lotnestyt@gmail.com>
2022-03-16 17:26:01 +0100

    Prevent dyno from idling

--- src/main/java/dev/lotnest/rikaapi/RikaApiApplication.java
+++ src/main/java/dev/lotnest/rikaapi/RikaApiApplication.java
@@ -1,13 +1,34 @@
 package dev.lotnest.rikaapi;
 
+import dev.lotnest.rikaapi.utils.TimeUtils;
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class RikaApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RikaApiApplication.class, args);
+        startDynoWakeUpTask();
+    }
+
+    @SneakyThrows
+    private static void startDynoWakeUpTask() {
+        URL url = new URL("https://rika-bot-api.herokuapp.com/");
+        TimeUtils.SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(() -> {
+                    try {
+                        url.openConnection();
+                    } catch (IOException ignored) {
+                    }
+                },
+                0L,
+                30L,
+                TimeUnit.MINUTES);
     }
 }
--- src/main/java/dev/lotnest/rikaapi/utils/TimeUtils.java
+++ src/main/java/dev/lotnest/rikaapi/utils/TimeUtils.java
@@ -1,11 +1,14 @@
 package dev.lotnest.rikaapi.utils;
 
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class TimeUtils {
 
     public static final DateTimeFormatter DEFAULT_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
     public static final DateTimeFormatter PLAN_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss'Z'");
+    public static final ScheduledExecutorService SCHEDULED_EXECUTOR_SERVICE = Executors.newSingleThreadScheduledExecutor();
 
     private TimeUtils() {
     }
