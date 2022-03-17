c591fcc77887b701dd23519b2b029f846b6a0690
Lotnest <lotnestyt@gmail.com>
2022-03-17 11:59:06 +0100

    Migrate dyno wake up task to discord bot

--- src/main/java/dev/lotnest/rikaapi/RikaApiApplication.java
+++ src/main/java/dev/lotnest/rikaapi/RikaApiApplication.java
@@ -1,38 +1,13 @@
 package dev.lotnest.rikaapi;
 
-import dev.lotnest.rikaapi.utils.TimeUtils;
-import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class RikaApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RikaApiApplication.class, args);
-        startDynoWakeUpTask();
-    }
-
-    @SneakyThrows
-    private static void startDynoWakeUpTask() {
-        URL url = new URL("https://rika-bot-api.herokuapp.com/");
-        TimeUtils.SCHEDULED_EXECUTOR_SERVICE.scheduleAtFixedRate(() -> {
-                    try {
-                        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                        httpURLConnection.setRequestMethod("GET");
-                        httpURLConnection.setConnectTimeout(30000);
-                        httpURLConnection.setReadTimeout(30000);
-                    } catch (IOException ignored) {
-                    }
-                },
-                0L,
-                20L,
-                TimeUnit.MINUTES);
     }
 }
